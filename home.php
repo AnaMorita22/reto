f56f5908cceb085669f86648c78b338fdd4009ce
Ana María <moraviedaanamaria@gmail.com>
2024-09-25 17:38:04 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,177 @@
+<?php
+    session_start(); // Asegúrate de iniciar la sesión
+    $userId = $_SESSION['user_id']; // Obtener el ID del usuario almacenado en la sesión
+    
+    include_once("conexion.php");
+
+    $conexion = new Conexion();
+    $db = $conexion->getConexion();
+
+    $query = "SELECT fecha_orden, documento_orden, numero_orden FROM ordenes WHERE user_id = $1"; // Asegúrate de que el campo user_id es el correcto
+$result = pg_query_params($conn, $query, array($user_id));
+
+if (!$result) {
+    echo "Error en la consulta: " . pg_last_error();
+    exit;
+} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@1,500&display=swap');
+    </style>
+</head>
+<body>
+<div class="container">
+
+<!-- Navegación -->
+<nav>
+    <a href="home.php"> <img src="img/hogar.png" alt="">Home</a>
+    <a href="perfil.php"><img src="img/perfil.png" alt="">Perfil de Usuario</a>
+</nav>
+
+<!-- Encabezado con logo, nombre del paciente y botón de cerrar sesión -->
+<div class="header">
+    <img src="img/R-FAST-PNG-05.png" alt="Logo">
+    <h1>Nombre del Paciente</h1>
+    <button onclick="redirigir()">Cerrar Sesión</button>
+</div>
+
+<!-- Contenido principal -->
+<div class="content">
+    <h2>Ordenes de Laboratorio</h2>
+    <div class="filtro">
+      <div class="numero">
+        <label for="NumeroOrden">Número de orden</label>
+        <input type="text" name="" id="">
+      </div>
+      <div class="fecha">
+        <label for="RangoFecha">Rango de Fecha</label>
+        <input type="date" name="fechaInicio" id="fechaInicio">
+        <input type="date" name="fechaFin" id="fechaFin">
+      </div>
+    </div>
+    <div class="orden">
+      <table>
+        <thead>
+            <tr>
+                <th>Fecha de la orden</th>
+                <th>Documento de la orden</th>
+                <th>Número de la orden</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+            <tr>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <td>XXXXXXXXXXXX</td>
+                <form action="orden.php" method="POST">
+                    <td class="actions"><button name="ver">Ver</button></td>
+                </form>
+                
+            </tr>
+        </tbody>
+    </table>
+
+    <!-- Paginación -->
+    <div class="pagination">
+        <button id="prevPage">&lt;</button>
+        <input type="text" id="numeroPagina" pattern="[0-9]+" value="1" min="1">
+        <button id="nextPage">&gt;</button>
+    </div>
+</div>
+    </div>
+</div>
+
+</div>
+    <script src="home.js"></script>
+</body>
+</html>
---
+++ orden.php
@@ -0,0 +1,90 @@
+<?php
+        if (isset($_POST['ver'])) {
+            
+        }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reto_MD</title>
+    <link rel="stylesheet" href="orden.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@1,500&display=swap');
+    </style>
+</head>
+<body>
+<nav>
+        <a href="home.php"><img src="img/hogar.png" alt=""> Home</a>
+        <a href="perfil.php"><img src="img/perfil.png" alt="">Perfil del usuario</a>
+    </nav>
+
+    <div class="main-content">
+        <div class="header">
+            <h1>Resultados de Laboratorio</h1>
+            <button class="logout-btn" onclick="redirigir()">Cerrar sesión</button>
+        </div>
+
+        <div class="patient-info">
+            <p><strong style="color: darkblue;">Paciente:</strong> XXXXXXXXXXXX</p>
+            <p><strong style="color: darkblue;">Identificación:</strong> XXXXXXXXXXXX</p>
+            <p><strong style="color: darkblue;">Teléfono:</strong> XXXXXXXXXXXX</p>
+            <p><strong style="color: darkblue;">Médico:</strong> XXXXXXXXXXXX</p>
+            <p><strong style="color: darkblue;">Servicio:</strong> XXXXXXXXXXXX</p>
+            <p><strong style="color: darkblue;">Fecha orden:</strong> XXXXXXXXXXXX</p>
+        </div>
+
+        <div class="lab-group">
+            <h2>Nombre del grupo (hematología)</h2>
+            <h3>Nombre del procedimiento</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Código de la prueba</th>
+                        <th>Nombre de la prueba</th>
+                        <th>Resultado</th>
+                        <th>Valores de Referencia</th>
+                        <th>Unidad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXX</td>
+                    </tr>
+                    <tr>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXX</td>
+                    </tr>
+                    <tr>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXX</td>
+                    </tr>
+                    <tr>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXXXX</td>
+                        <td>XXXXX</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <p style="color: rgb(1, 1, 75);">XXXXXXXXXXXXXXXXXXXX</p>
+    </div>
+
+    <script src="orden.js"></script>
+</body>
+</html>
